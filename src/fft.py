ea60efd01e410763140449941e8af1e95b4a7fc2
XOR505 <clemgori@hotmail.fr>
2013-11-25 10:47:43 +0100

    Update fft.py

--- src/fft.py
+++ src/fft.py
@@ -13,7 +13,8 @@ import matplotlib.pyplot as plt
 
 def fft(sig, sig2=[], meth="CT", mid=True, lin=True, freqMax=12000):
     """param sig : signal a traiter
-        param sig2=[] : !!! EN CHANTIER !!! : signal secondaire à calculer en parallele
+        param sig2=[] : signal secondaire à calculer en parallele. Donne un 
+                        tuple avec les deux spectres.
         param meth="CT" : methode de calcul de la FFT
             CT : Cooley-Tukey (classique, par defaut)
         param mid=True : renvoyer la moitie du tableau (parite du spectre)
@@ -21,23 +22,30 @@ def fft(sig, sig2=[], meth="CT", mid=True, lin=True, freqMax=12000):
         param freqMax=12000 : frequence maximale representee dans le tableau"""
     N = len(sig)
     M = len(sig2)
-    if meth=="CT":
-        if sig2==[]: C = fftCT(sig,lin)
-        else: C = fftCT([sig[k]+sig2[k]*1j for k in range(N)],lin)
-    if sig2==[]:
-        if mid: return C[len(C)/2:]
-        else: return C
+    if (M!=0 and M==N):
+        if meth=="CT":
+            if sig2==[]: C = fftCT(sig,lin)
+            else: C = fftCT([sig[k]+sig2[k]*1j for k in range(N)],lin)
+        if sig2==[]:
+            if mid:
+                return C[len(C)/2:]
+            else:
+                return C
+        else:
+            C1 = [(C[k]+C[N-k].conjugate())/2 for k in range(1,N)]
+            C2 = [(C[k]-C[N-k].conjugate())/(2j) for k in range(1,N)]
+            if mid: return C1[len(C1)/2:],C2[len(C2)/2:]
+            else: return C1,C2
     else:
-        C1 = [(C[k]+C[N-k].conjugate())/2 for k in range(N)]
-        C2 = [(C[k]-C[N-k].conjugate())/(2j) for k in range(N)]
-        if mid: return C1[len(C1)/2:],C2[len(C2)/2:]
-        else: return C1,C2
+        print "Deux echantillons de meme taille needed !"
+        return [],[]
 
 def fftCT(sig,lin):
     N = len(sig)
     if util.is2Power(N):
         return fftCTRec(sig)
     else:
+        print "zPad needed"
         return fftCTRec(util.zPad(sig))
 
 def fftCTRec(sig):
@@ -49,7 +57,10 @@ def fftCTRec(sig):
         odd = fftCTRec(sig[1::2])
         return [even[k] + np.exp(-2j*np.pi*k/N)*odd[k] for k in range(N/2)] + \
                [even[k] - np.exp(-2j*np.pi*k/N)*odd[k] for k in range(N/2)]
+
                
-
-     
-
+freq1,freq2 = 220,440
+freqEch = 44100
+s1 = [np.sin(2*np.pi*freq1*t/freqEch) for t in range(1023)]
+s2 = [np.sin(2*np.pi*freq2*t/freqEch) for t in range(1024)]
+C1,C2 = fft(s1,s2)
